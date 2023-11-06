b51fb2b6fafb2060d16885fb5411750cfa538d05
Santiago Rojas Tunjano <srojastunjano@gmail.com>
2023-11-06 13:21:49 +0000

    Chatgpt now works on the website.

--- ChatGPT.py
+++ ChatGPT.py
@@ -3,48 +3,29 @@ import os
 import openai
 from dotenv import load_dotenv
 
-load_dotenv("Environment.env")
+load_dotenv()
 app = Flask(__name__) # os is being used to retrieve the environment-variable: OpenAI API key. 
 
-openai_api_key = os.getenv("OPENAI_API_KEY")
-organization_id = os.getenv("ID")
+API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = API_KEY 
 
-@app.route("/", methods=["GET","POST"])
+@app.route('/', methods=["GET", "POST"])
 def index():
-  completion = openai.ChatCompletion.create(   #the completion variable hold the JSON responce that the ChatGPT API recieves
-      model="gpt-3.5-turbo",
-      temperature = "0.8",
-      messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {"role": "user", "content": "Hello!"}
-      ]
-    )
+    gpt_answer = "No Answer yet"
+    if request.method == 'POST':
+      print(user_input)
+      completion = openai.ChatCompletion.create(   #the completion variable hold the JSON responce that the ChatGPT API recieves
+        model="gpt-3.5-turbo",
+        temperature = 0.8,
+        messages=[
+          {"role": "system", "content": "You are a helpful assistant."},
+          {"role": "user", "content": request.form["user_input"]}
+        ]
+      )
+      print(completion)
+      gpt_answer = completion["choices"][0]["message"]["content"]
+      
 
-  if request.method == 'POST':
-    completion["messages"][1]["content"] = request.form["user_input"]
-    ChatGPT = completion["messages"][0]["content"]
-    return render_template("main.html", ChatGPT=ChatGPT)
+    return render_template('main.html', gpt_answer=gpt_answer)
 
-  
-app.run(host="0.0.0.0", port=5000)
-
-
-#Example of the responce I would get from ChatGPT API
-# {   
-#   "id": "chatcmpl-123",
-#   "object": "chat.completion",
-#   "created": 1677652288,
-#   "model": "gpt-3.5-turbo-0613",
-#   "choices": [{
-#     "index": 0,
-#     "message": {
-#       "role": "assistant",
-#       "content": "\n\nHello there, how may I assist you today?",
-#     },
-#     "finish_reason": "stop"
-#   }],
-#   "usage": {
-#     "prompt_tokens": 9,
-#     "completion_tokens": 12,
-#     "total_tokens": 21
-#   }
+app.run()
